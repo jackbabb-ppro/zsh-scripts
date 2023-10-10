9ba8b6f1c7a914abdebf9f3e1ff74d9747cdb40d
Jack Babb <jbabb@aptean.com>
2023-10-10 15:50:34 -0500

    Add a more generic function to run git commands on all repos in a dir

--- bash/git-fetch-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-if [ $# -lt 1 ]; then
-	echo "Fetches all git repos in a folder"
-	echo "Usage: $0 <parent folder>"
-	return;
-fi
-cd $1
-find . -type d -maxdepth 1 -exec git --git-dir={}/.git --work-tree=$1/{} fetch \;
